a4e4a13e903f11b3df84d6e83ce276335096bac7
oscar <oscar@dw.zjpjhx.gmail.com.beta.tailscale.net>
2022-12-05 15:43:35 +0800

    add packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 install_requires = [
     "ccxt>=1.95.13",
@@ -13,8 +13,9 @@ install_requires = [
 setup(
     name='kline-redis',
     description="K 线数据 redisk 缓存",
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['kline'],
+    packages=find_packages(exclude=["tests"]),
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
