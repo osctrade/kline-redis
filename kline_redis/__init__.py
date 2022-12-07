6ebf4288936d7336c624be0e9b8b257db48c7e29
oscar <oscar@dw.local>
2022-12-07 11:02:06 +0800

    add get_exchange_info

--- kline_redis/__init__.py
+++ kline_redis/__init__.py
@@ -1,1 +1,1 @@
-from kline_redis.api import get_symbols, get_current_price, get_kline, kline_check, get_redis
+from kline_redis.api import get_symbols, get_current_price, get_kline, kline_check, get_redis, get_exchange_info
