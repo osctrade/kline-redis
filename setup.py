9e9b56ba9ec3230c720640a47ef852583cf0ec03
oscar <oscar@dw.tail525ed.ts.net>
2022-12-09 08:49:16 +0800

    dis proxy

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 setup(
     name='kline-redis',
     description="K 线数据 redisk 缓存",
-    version='0.1.2',
+    version='0.1.3',
     py_modules=['kline_redis'],
     packages=find_packages(exclude=["tests"]),
     install_requires=install_requires,
